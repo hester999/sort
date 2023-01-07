78c699ce7b413474e085804c7769ff355fccc085
123 123 <ivankuzin09@gmail.com>
2023-01-07 18:08:16 +0300

    Добавьте файлы проекта.

---
+++ sort/Program.cs
@@ -0,0 +1,17 @@
+﻿using stack;
+
+
+Stack s = new Stack();
+
+s.Push(5);
+
+s.Push(3);
+
+
+
+s.Set_element(0, 2);
+
+s.Show();
+
+
+
---
+++ stack/Class1.cs
@@ -0,0 +1,43 @@
+﻿namespace stack
+{
+    public class Stack
+    {
+        private List<int> stack;
+
+        public Stack()
+        {
+            stack = new List<int>();
+        }
+
+        public void Push(int i)
+        {
+            stack.Add(i);
+        }
+        public int Len()
+        {
+            return stack.Count;
+        }
+
+        public void Show()
+        {
+
+            stack.Reverse();
+
+            foreach (int item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        public int Get_element(int item)
+        {
+            return stack[item];
+        }
+
+        public void Set_element(int item, int index)
+        {
+            stack.Insert(index, item);
+        }
+    }
+}
