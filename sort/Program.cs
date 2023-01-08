1dbdeee67c7906ba6a6acf6c8eae5030ed94fac0
hester <ivankuzin09@gmail.com>
2023-01-08 17:35:19 +0300

    .

--- sort/Program.cs
+++ sort/Program.cs
@@ -1,17 +1,89 @@
-﻿using stack;
+﻿using System.Formats.Asn1;
+using stack;
 
 
 Stack s = new Stack();
 
-s.Push(5);
+bool flag = true;
 
-s.Push(3);
+while (flag)
+{
+    Console.WriteLine("1. Add element in stack");
+    Console.WriteLine("2. Show element by index");
+    Console.WriteLine("3. Show stack lenght");
+    Console.WriteLine("4. Set the element by index");
+    Console.WriteLine("5. Get the element by index");
+    Console.WriteLine("6. Show all stack");
+    Console.WriteLine("7. Sort stack");
+    Console.WriteLine("8. Clear all stack");
+    Console.WriteLine("9. Exit");
+    Console.Write("Choose point menu: ");
+
+    int answer = Int32.Parse(Console.ReadLine());
+
+    switch (answer)
+    {
+        case 1:
+            Console.WriteLine("do you want to enter several elements? y/n");
+            string ans = Console.ReadLine();
+            if (ans == "y" || ans == "Y")
+            {
+                Console.WriteLine("enter stack lenght");
+                int stack_len = Int32.Parse(Console.ReadLine());
+
+                for (int i = 0; i < stack_len; i++)
+                {
+                    Console.WriteLine("Enter element");
+                    int element  = Int32.Parse(Console.ReadLine());
+                    s.Push(element);
+                }
+            }
+            else
+            {int item = Int32.Parse(Console.ReadLine());
+                s.Push(item);
+            }
+            break;  
+        case 2:
+            Console.WriteLine("Enter index");
+            int index = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(s.Set(index));
+            break;
+            
+        case 3:
+            Console.WriteLine(s.Len());
+            break;
+        case 4:
+            Console.WriteLine("enter element");
+            int element_1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter position");
+            int pos = Int32.Parse(Console.ReadLine());
+            
+            s.Set_element(element_1 , pos);
+            break;
+        case 5: 
+            Console.WriteLine("Enter index");
+            int index_1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(s.Get_element(index_1));
+            break;
+        case 6:
+            s.Show();
+            break;
+        case 7:
+            s.BinaryInsertionSort();
+            s.Show();
+            break;
+        case 8:
+            s.Clear();
+            break;
+        case 9:
+            flag = false;
+            break;
+    }
+}
 
 
 
-s.Set_element(0, 2);
 
-s.Show();
 
 
 
--- stack/Class1.cs
+++ stack/Class1.cs
@@ -16,6 +16,7 @@
         public int Len()
         {
             return stack.Count;
+            
         }
 
         public void Show()
@@ -25,11 +26,17 @@
 
             foreach (int item in stack)
             {
-                Console.WriteLine(item);
+                Console.Write(item);
+                Console.Write(" ");
             }
-
+             
+            Console.WriteLine();
         }
 
+        public int Set(int item)
+        {
+            return stack[item];
+        }
         public int Get_element(int item)
         {
             return stack[item];
@@ -38,6 +45,43 @@
         public void Set_element(int item, int index)
         {
             stack.Insert(index, item);
+        }
+
+        public void Clear()
+        {
+            stack.Clear();
+        }
+
+
+     
+        public void BinaryInsertionSort()
+        {
+            for (int i = 1; i < stack.Count; ++i) {
+                int key = stack[i];
+                int insertedPosition = findPosition(stack, 0, i - 1, key);
+
+                for (int j = i - 1; j >= insertedPosition; --j) {
+                    stack[j + 1] = stack[j];
+                }
+
+                stack[insertedPosition] = key;
+            }
+
+            stack.Reverse();
+        }
+
+        public static int findPosition(List<int> nums, int start, int end, int key) {
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+
+                if (key < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+
+            return start;
         }
     }
 }
